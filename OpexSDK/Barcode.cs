abc3aa73d0aa9f98dbd7f48949dd9c502409f5c6
Lee Dumond <leedumond@gmail.com>
2019-06-14 14:16:11 -0500

    define Barcode properties

--- OpexSDK/Barcode.cs
+++ OpexSDK/Barcode.cs
@@ -1,6 +1,12 @@
-﻿namespace OpexSDK
+﻿using OpexSDK.Enumerations;
+
+namespace OpexSDK
 {
     public class Barcode
     {
+        public int Index { get; set; }
+        public string Type { get; set; }
+        public Side Side { get; set; }
+        public string Value { get; set; }
     }
 }
