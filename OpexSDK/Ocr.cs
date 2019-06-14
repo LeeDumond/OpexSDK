8a967d20e3c5887eacfa0d2666f599c92d24a256
Lee Dumond <leedumond@gmail.com>
2019-06-14 14:12:44 -0500

    define properties for ocr

--- OpexSDK/Ocr.cs
+++ OpexSDK/Ocr.cs
@@ -1,6 +1,12 @@
-﻿namespace OpexSDK
+﻿using OpexSDK.Enumerations;
+
+namespace OpexSDK
 {
     public class Ocr
     {
+        public int Index { get; set; }
+        public Side Side { get; set; }
+        public string Value { get; set; }
+        public string Name { get; set; }
     }
 }
