72ecc68315c5e543bd9c3bde120b85881067cd40
Lee Dumond <leedumond@gmail.com>
2019-06-14 14:35:51 -0500

    define properties for TagSource

---
+++ OpexSDK/Enumerations/TagSource.cs
@@ -0,0 +1,18 @@
+﻿namespace OpexSDK.Enumerations
+{
+    public enum TagSource
+    {
+        I1,
+        M1,
+        M2,
+        M3,
+        M4,
+        M5,
+        M6,
+        M7,
+        M8,
+        M9,
+        MfdOverride,
+        ExternalCamera
+    }
+}
--- OpexSDK/Tag.cs
+++ OpexSDK/Tag.cs
@@ -1,6 +1,10 @@
-﻿namespace OpexSDK
+﻿using OpexSDK.Enumerations;
+
+namespace OpexSDK
 {
     public class Tag
     {
+        public TagSource Source { get; set; }
+        public string Value { get; set; }
     }
 }
