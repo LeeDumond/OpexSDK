9b08ad12875c8ee6a19189125600a11b6e8c7ad5
Lee Dumond <leedumond@gmail.com>
2019-06-14 14:39:09 -0500

    define properties for EndINfo

--- OpexSDK/EndInfo.cs
+++ OpexSDK/EndInfo.cs
@@ -1,6 +1,13 @@
-﻿namespace OpexSDK
+﻿using System;
+
+namespace OpexSDK
 {
     public class EndInfo
     {
+        public DateTime EndTime { get; set; }
+        public int NumPages { get; set; }
+        public int NumGroups { get; set; }
+        public int NumTransactions { get; set; }
+        public bool IsModified { get; set; }
     }
 }
