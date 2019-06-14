b954bc23ce8c2fd8e3d43c54bafa2fa7f7eadd27
Lee Dumond <leedumond@gmail.com>
2019-06-14 14:23:38 -0500

    define MarkDetect properties

--- OpexSDK/MarkDetect.cs
+++ OpexSDK/MarkDetect.cs
@@ -1,6 +1,12 @@
-﻿namespace OpexSDK
+﻿using OpexSDK.Enumerations;
+
+namespace OpexSDK
 {
     public class MarkDetect
     {
+        public int Index { get; set; }
+        public Side Side { get; set; }
+        public bool Result { get; set; }
+        public string Name { get; set; }
     }
 }
