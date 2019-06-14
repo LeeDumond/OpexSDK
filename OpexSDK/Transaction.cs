d0809ef2499d6c6666daf979677d8b7e2b0e372d
Lee Dumond <leedumond@gmail.com>
2019-06-14 15:36:16 -0500

    documentation for Transaction

--- OpexSDK/Transaction.cs
+++ OpexSDK/Transaction.cs
@@ -2,10 +2,19 @@
 
 namespace OpexSDK
 {
+    /// <summary>
+    /// Groups together a collection of Group elements into a logical group. A transaction is envisioned to consist of the contents of a folder of pages.
+    /// </summary>
     public class Transaction
     {
+        /// <summary>
+        /// Transaction in batch. Batch tickets always have a Transaction Number of zero. All pages in a transaction have the same Transaction Number. The Transaction Number of the first non-batch ticket transaction may be 1 or another larger number as defined in the job file.
+        /// </summary>
         public int TransactionId { get; set; }
 
+        /// <summary>
+        /// One for each group within the transaction.
+        /// </summary>
         public ICollection<Group> Groups { get; }
     }
 }
