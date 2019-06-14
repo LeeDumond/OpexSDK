0e1d9913b78fa615a50aef180adeb56e32ab060c
Lee Dumond <leedumond@gmail.com>
2019-06-14 15:13:22 -0500

    xml docs for Batch

--- OpexSDK/Enumerations/JobType.cs
+++ OpexSDK/Enumerations/JobType.cs
@@ -1,15 +1,48 @@
 ﻿namespace OpexSDK.Enumerations
 {
+    /// <summary>
+    /// Type of transactions in job.
+    /// </summary>
     public enum JobType
     {
-        Unknown,
+        /// <summary>
+        /// One stub, one check
+        /// </summary>
         Single,
+
+        /// <summary>
+        /// One or more stubs, one or more checks
+        /// </summary>
         Multi,
+
+        /// <summary>
+        /// One stub
+        /// </summary>
         StubOnly,
+
+        /// <summary>
+        /// One check
+        /// </summary>
         CheckOnly,
+
+        /// <summary>
+        /// One or more stubs, one or more checks, zero or more pages
+        /// </summary>
+        /// 
         MultiWithPage,
+        /// <summary>
+        /// one or more pages
+        /// </summary>
         PageOnly,
+
+        /// <summary>
+        /// Any documents
+        /// </summary>
         Unstructured,
+
+        /// <summary>
+        /// Transaction validation determined by customized job settings
+        /// </summary>
         Structured
     }
 }
--- OpexSDK/Enumerations/OperatingMode.cs
+++ OpexSDK/Enumerations/OperatingMode.cs
@@ -1,8 +1,18 @@
 ﻿namespace OpexSDK.Enumerations
 {
+    /// <summary>
+    /// Operating mode of OPEX Scanning Device.
+    /// </summary>
     public enum OperatingMode
     {
+        /// <summary>
+        /// Refers to AS3600 manual drop mode.
+        /// </summary>
         ManualScan,
+
+        /// <summary>
+        /// This batch has been modified.
+        /// </summary>
         Modified
     }
 }
