acf5ff27d83e25387d631e540ffe67371891dbdb
Lee Dumond <leedumond@gmail.com>
2019-06-15 11:45:33 -0500

    documentation for MarkDetect

--- OpexSDK/MarkDetect.cs
+++ OpexSDK/MarkDetect.cs
@@ -2,11 +2,30 @@
 
 namespace OpexSDK
 {
+    /// <summary>
+    /// Contains information about a single Optical Mark Detect read.
+    /// Optical Mark Detection can be used to look for Change of Address (CHAD), checkboxes, etc.
+    /// </summary>
     public class MarkDetect
     {
+        /// <summary>
+        /// The index of the Mark read, as configured in the job. Values are 1-indexed. (i.e., "1" is the first index.)
+        /// </summary>
         public int Index { get; set; }
+
+        /// <summary>
+        /// Side on which the Mark Detect read was performed.
+        /// </summary>
         public Side Side { get; set; }
+
+        /// <summary>
+        /// Indicates the outcome of the Mark detection. True if a mark was found; otherwise false. 
+        /// </summary>
         public bool Result { get; set; }
+
+        /// <summary>
+        /// Reiterates the index of the Mark read, i.e. if Index is x, this value will be 'MARK x'.
+        /// </summary>
         public string Name { get; set; }
     }
 }
