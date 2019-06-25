bf38711b950eb623d0b0ec1a8a351936bbe81369
Lee Dumond <leedumond@gmail.com>
2019-06-24 21:28:00 -0500

    adding a comsole demo app

---
+++ OpexSDK.ConsoleDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpexSDK.Enumerations;
+using OpexSDK.Models;
+
+namespace OpexSDK.ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BatchReader reader = new BatchReader("sample.oxi", "oxi1_60.xsd");
+
+            Batch batch = reader.ReadBatch();
+
+            Console.WriteLine("Batch:");
+            Console.WriteLine($"\tFormat Version: {batch.FormatVersion}");
+            Console.WriteLine($"\tBase Machine: {batch.BaseMachine}");
+            Console.WriteLine($"\tScan Device: {batch.ScanDevice}");
+            Console.WriteLine($"\tSoftware Version: {batch.SoftwareVersion}");
+            Console.WriteLine($"\tTransport ID: {batch.TransportId}");
+            Console.WriteLine($"\tBatch Identifier: {batch.BatchIdentifier}");
+            Console.WriteLine($"\tJob Type: {batch.JobType}");
+            Console.WriteLine($"\tOperating Mode: {batch.OperatingMode}");
+            Console.WriteLine($"\tJobName: {batch.JobName}");
+
+            
+        }
+    }
+}
